3f7d689609c623ca598f13eed5aa356eb42d7ee4
Angelill014 <angelsanchezdelacruz@gmail.com>
2022-02-02 13:19:23 +0100

    Las hamburguesas del MC Donald´s estan buenisismas

---
+++ PROYECTO CONSOLA/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace PROYECTO_CONSOLA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string mensajeDeSalida;
+           mensajeDeSalida = "Me gustan las motos";
+        Console.WriteLine(mensajeDeSalida);
+
+            mensajeDeSalida = "Estoy en el edificio B";
+            Console.Write(mensajeDeSalida);
+            mensajeDeSalida = Console.ReadLine();
+            Console.WriteLine("Maldito Coronavirus" + mensajeDeSalida);
+            Console.ReadLine();
+
+           
+
+        }   
+    }  
+}
